fb7f8f4bb9392cec73193be83832984d24201e03
Maegima <maegima.andre@gmail.com>
2019-12-10 17:54:51 -0300

    classe TokenType

---
+++ utils/TokenType.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file TokenType.hpp
+ * @author André Lucas Maegima
+ * @brief Definição da classe TokenType.
+ * @version 1.0
+ * @date 2019-12-10
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#ifndef _TOKENTYPE_HPP_
+#define _TOKENTYPE_HPP_
+
+/** @brief Yacc/Bison gera o próprio tipo para valores de token. */
+typedef int TokenType; 
+
+#endif
